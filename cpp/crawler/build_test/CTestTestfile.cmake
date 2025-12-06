30d56c5d2ceb9c6eda0aa99505893624f3a07ace
Digvijay Singh Rawat <hackerearthx1@gmail.com>
2025-12-06 16:10:04 +0530

    fix: remove build artifacts and update gitignore

--- cpp/crawler/build_test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/digvijay/coding/project/Search-Engine/cpp/crawler/src
-# Build directory: /home/digvijay/coding/project/Search-Engine/cpp/crawler/build_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(WarcWriterTest "/home/digvijay/coding/project/Search-Engine/cpp/crawler/build_test/test_crawler")
-set_tests_properties(WarcWriterTest PROPERTIES  _BACKTRACE_TRIPLES "/home/digvijay/coding/project/Search-Engine/cpp/crawler/src/CMakeLists.txt;29;add_test;/home/digvijay/coding/project/Search-Engine/cpp/crawler/src/CMakeLists.txt;0;")
